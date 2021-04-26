7fc1e377f38e20e6cc5c3d4564cab6ccd6e30960
gulsude <gulsudedemircan@gmail.com>
2021-04-26 11:15:49 +0200

    Import Data

--- R/02_clean.R
+++ R/02_clean.R
@@ -4,20 +4,20 @@ rm(list = ls())
 
 # Load libraries ----------------------------------------------------------
 library("tidyverse")
-
+library("readr")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
 
 
 # Load data ---------------------------------------------------------------
-my_data <- read_tsv(file = "data/01_my_data.tsv")
+data <- read_csv("data/_raw/prostateCancerData.csv")
 
 
 # Wrangle data ------------------------------------------------------------
-my_data_clean <- my_data # %>% ...
+data_clean <- data # %>% ...
 
 
 # Write data --------------------------------------------------------------
-write_tsv(x = my_data_clean,
-          file = "data/02_my_data_clean.tsv")
+write_tsv(x = data_clean,
+          file = "data/02_data_clean.tsv")
