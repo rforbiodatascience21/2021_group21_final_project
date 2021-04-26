28747aa7798853a00b75ee628a0030c907dbf60e
dayadmg <daya.dmg@gmail.com>
2021-04-26 12:35:31 +0000

    s

--- R/03_augment.R
+++ R/03_augment.R
@@ -15,7 +15,7 @@ data_clean <- read_tsv(file = "data/02_data_clean.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
 
-data_clean_aug <- data_clean %>% separate(status,
+data_clean <- data_clean %>% separate(status,
                                           c("status","reasonDeath"),
                                           " - ") %>% 
   mutate(dose = case_when(str_detect(treatment, pattern = "estrogen") ~ str_sub(treatment, start = 1, end = 3),
@@ -25,19 +25,12 @@ data_clean_aug <- data_clean %>% separate(status,
   mutate(dose = as.numeric(dose))
 
 
-glimpse(data_clean_aug)
 
-data_clean_aug <- data_clean %>% 
-  mutate(status = case_when(status == "alive" ~ 0,
-                            status == "dead - prostatic ca" ~ 1,
-                            status == "dead - heart or vascular" ~ 2,
-                            status == "dead - cerebrovascular" ~ 3,
-                            status == "dead - pulmonary embolus" ~ 4,
-                            status == "dead - other ca" ~ 5,
-                            status == "dead - respiratory disease" ~ 6,
-                            status == "dead - other specific non-ca" ~ 7,
-                            status == "dead - unspecified non-ca" ~ 8,
-                            status == "dead - unknown cause" ~ 9))
+
+
+glimpse(data_clean)
+
+
 
   
 
