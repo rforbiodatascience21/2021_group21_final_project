4e5ca3d75eff1f9736aaa3721662e07f8ea1fe0a
gulsude <gulsudedemircan@gmail.com>
2021-04-26 11:49:23 +0200

    raw_data dataset is ready to clean

--- R/02_clean.R
+++ R/02_clean.R
@@ -15,25 +15,20 @@ raw_data <- read_tsv(file = "data/01_my_data.tsv.gz")
 
 
 # Wrangle data ------------------------------------------------------------
-<<<<<<< HEAD
 
-data %>% 
+raw_data %>% 
   rename(patno = patientID, rx = treatment, dtime = monthFollowUp, wt = weightIndex, hx = historyCardio, sbp = systolicBP, dbp = diastolicBP, ekg = electroCardioG, hg= hemoglobin, sz= tumorSize, sg = SGindex, ap = acidPhosphatase, bm = boneMetastase)
 
-
-glimpse(data)
-
-=======
-
 glimpse(raw_data)
->>>>>>> 6653292e395d24fa0fad032f0ecb20ac3634d7a3
 
 NA_values ->sum(is.na(raw_data)) # 27 NA values
+
+raw_data %>% drop_na()
 
 raw_data %>%
   summarise(n = n_distinct(patno))  # 502 rows, 502 unique patients, so no duplicates in patients
 
-data_clean <- data %>%
+data_clean <- raw_data %>%
 
 # Write data --------------------------------------------------------------
 write_tsv(x = data_clean,
