d57fc6d5e92ab233d339b8923fa1f1a47d61ce1b
enricCristobal <enric525@gmail.com>
2021-05-09 13:12:24 +0200

    Further PCA analysis

--- R/07_PCA_analysis.R
+++ R/07_PCA_analysis.R
@@ -8,6 +8,7 @@ library("ggridges")
 library("ggplot2")
 library("ggridges")
 library("broom")
+library("factoextra")
 
 # Define functions --------------------------------------------------------
 source(file = "R/99_project_functions.R")
@@ -17,24 +18,19 @@ source(file = "R/99_project_functions.R")
 data_clean_num <- read_tsv(file = "data/03_data_clean_num.tsv.gz")
 
 # Wrangle data ------------------------------------------------------------
-data_clean_num <- data_clean_num %>% 
-  mutate(status = as_factor(status)) %>% 
-  mutate(historyCardio = as_factor(historyCardio)) %>%
-  mutate(electroCardioG = as_factor(electroCardioG)) %>%
-  mutate(performance = as_factor(performance)) %>%
-  mutate(stage = as_factor(stage)) %>% 
-  mutate(boneMetastase = as_factor(boneMetastase))
 
-# we run the PCA and plot the results
+# Run a PCA with just the continuous attributes
 
 dropCol <- c("performance", "historyCardio", "boneMetastase", "stage", "electroCardioG")
 
-pca_data <- data_clean_num %>% 
+pca1_data <- data_clean_num %>% 
   select(-one_of(dropCol)) %>%
   select(!status) %>%
   prcomp(scale = TRUE)
 
-plot <- pca_data %>% augment(data_clean_num) %>%
+fviz_eig(pca1_data)
+
+PCA1 <- pca1_data %>% augment(data_clean_num) %>%
   mutate(status = factor(status)) %>%
   ggplot(aes(x = .fittedPC1,
              y = .fittedPC2,
@@ -42,8 +38,34 @@ plot <- pca_data %>% augment(data_clean_num) %>%
   geom_point(size = 2) + 
   xlab("PC1") +
   ylab("PC2") + 
-  theme_classic(base_family = "Avenir", base_size = 8) +
+  theme_classic(base_size = 14) +
   theme(legend.position = "bottom")
-plot
+
+PCA1_contribution <- fviz_pca_var(pca1_data,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+# Run a PCA with all variables
+
+pca2_data <- data_clean_num %>% 
+  select(!status) %>%
+  prcomp(scale = TRUE)
+
+PCA2 <- pca2_data %>% augment(data_clean_num) %>%
+  mutate(status = factor(status)) %>%
+  ggplot(aes(x = .fittedPC1,
+             y = .fittedPC2,
+             color = status)) + 
+  geom_point(size = 2) + 
+  xlab("PC1") +
+  ylab("PC2") + 
+  theme_classic(base_size = 14) +
+  theme(legend.position = "bottom")
+
 # Write data --------------------------------------------------------------
-ggsave(plot, file = "results/07_PCA.png")
+ggsave(PCA1, file = "results/07_PCA_continuous.png")
+ggsave(PCA1_contribution, file = "results/07_PCA_contribution.png")
+ggsave(PCA2, file = "results/07_PCA_all_variables.png")
+
