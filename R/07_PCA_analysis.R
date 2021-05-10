f9a190fbec7549c1e0ed6b1efc723c54b246b6e7
unknown <daya.dmg@gmail.com>
2021-05-10 18:30:04 +0200

    plot legend

--- R/07_PCA_analysis.R
+++ R/07_PCA_analysis.R
@@ -28,7 +28,8 @@ pca1_data <- data_clean_num %>%
 PCA_variance <- fviz_eig(pca1_data)
 
 PCA1 <- pca1_data %>% augment(data_clean_num) %>%
-  mutate(status = factor(status)) %>%
+  mutate(status = case_when(status == 0 ~ "Alive",
+                            status == 1 ~ "Dead")) %>%
   ggplot(aes(x = .fittedPC1,
              y = .fittedPC2,
              color = status)) + 
@@ -40,7 +41,7 @@ PCA1 <- pca1_data %>% augment(data_clean_num) %>%
 
 PCA1_contribution <- fviz_pca_var(pca1_data,
                                   col.var = "contrib", # Color by contributions to the PC
-                                  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+                                  #gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
                                   repel = TRUE)     # Avoid text overlapping
 
 # Run a PCA with all variables
