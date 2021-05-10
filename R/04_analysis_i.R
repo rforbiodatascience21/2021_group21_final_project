6bcd51d1528dff6fa24b6090e6a7d9a8d664ccda
gulsude <gulsudedemircan@gmail.com>
2021-05-10 10:29:35 +0200

    organize file according to the tidyverse style.

--- R/04_analysis_i.R
+++ R/04_analysis_i.R
@@ -32,20 +32,26 @@ data_clean_aug <- data_clean_aug %>%
 #Plot 0 - very basic, see the count of stage 3 and 4, we may exclude this
 plot0 <- data_clean_aug %>%
   ggplot(aes(stage, ..count..)) + 
-  geom_bar(alpha=0.7,aes(fill = stage), position = "dodge") +
-  labs(x= "Cancer stage", y="Total patients") +
+  geom_bar(alpha = 0.7, 
+           aes(fill = stage), 
+           position = "dodge") +
+  labs(x = "Cancer stage", 
+       y = "Total patients") +
   ggtitle("Total patients depending on cancer stage") +
-  geom_text(stat='count', aes(label=..count..), vjust=-1) +
+  geom_text(stat = 'count', 
+            aes(label = ..count..), 
+            vjust = -1) +
   ylim(c(0,300)) +
-  scale_fill_manual(values=c("#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
   theme_minimal() +
   theme(legend.position = "none")
 
+
 #Plot 1 - tumor Size vs cancer Stage
 plot1 <- data_clean_aug %>%
-  ggplot( aes(y=stage, x=tumorSize,  fill=stage)) +
+  ggplot(aes(y = stage, x = tumorSize, fill = stage)) +
   geom_density_ridges(alpha=0.7) +
-  scale_fill_manual(values=c("#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
   ggtitle("Tumor Size Density With Cancer Stages") +
   xlab("Tumor Size (cm2)") +
   ylab("Cancer Stage") + 
@@ -59,68 +65,94 @@ plot2 <- data_clean_aug %>%
            reasonDeath != "other ca" & 
            reasonDeath != "other non-ca") %>% 
   count(reasonDeath, stage) %>% 
-  ggplot(aes(x = reorder(reasonDeath, n, sum), y = n, fill = stage)) + 
+  ggplot(aes(x = reorder(reasonDeath, n, sum), 
+             y = n, 
+             fill = stage)) + 
   geom_col() +
-  scale_fill_manual(values=c( "#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c( "#00AFBB", "#E7B800")) +
   ggtitle("Death Reason - Grouped By Cancer Stage") +
   xlab("Reason of Death") +
   ylab("Total patients") +   
   theme_minimal() +
-  theme(axis.text.x = element_text(angle = 25, vjust = 0.8, hjust=0.5, size=11))
+  theme(axis.text.x = element_text(angle = 25, 
+                                   vjust = 0.8, 
+                                   hjust = 0.5, 
+                                   size = 11))
 
 
 #Plot 3 - stage vs acid phosphates
 p1 <- data_clean_aug %>% 
-  ggplot(aes(x=stage, y=log(acidPhosphatase), fill= stage)) +
+  ggplot(aes(x =stage, 
+             y = log(acidPhosphatase), 
+             fill = stage)) +
   geom_violin() +
-  stat_summary(fun = mean, fun.min = mean, fun.max = mean,
+  stat_summary(fun = mean, 
+               fun.min = mean, 
+               fun.max = mean,
                geom = "crossbar", 
                width = 0.25,
                position = position_dodge(width = .25)) +
-  labs(x= "Cancer stage", y="Log of acid phosphatase", color="Cancer stage") +
+  labs(x = "Cancer stage", 
+       y = "Log of acid phosphatase", 
+       color = "Cancer stage") +
   ggtitle("Violin Plot of Acid Phosphatases and SG Index") +
-  scale_fill_manual(values=c("#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
   theme_minimal() +
   theme(legend.position = "none")
+
 
 p2 <-  data_clean_aug %>% 
-  ggplot(aes(x=stage, y=SGindex, fill= stage)) +
+  ggplot(aes(x = stage, 
+             y = SGindex, 
+             fill = stage)) +
   geom_violin() +
-  stat_summary(fun = mean, fun.min = mean, fun.max = mean,
+  stat_summary(fun = mean, 
+               fun.min = mean, 
+               fun.max = mean,
                geom = "crossbar", 
                width = 0.25,
                position = position_dodge(width = .25)) +
-  labs(x= "Cancer stage", y="SG Index", color="Cancer stage") +
-  scale_fill_manual(values=c("#00AFBB", "#E7B800")) +
+  labs(x = "Cancer stage", 
+       y ="SG Index", 
+       color ="Cancer stage") +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
   theme_minimal() +
   theme(legend.position = "none")
 
+
 plot3 <- p1 + p2
+
 
 #plot 4 - survival depending on treatment and by stage
 plot4 <- data_clean_aug %>%
   filter(reasonDeath != "unknown cause" & 
            reasonDeath != "other ca" & 
            reasonDeath != "other non-ca") %>% 
-  count(dose,status,stage) %>% 
+  count(dose,
+        status,
+        stage) %>% 
   mutate(stage = case_when(stage == 3 ~ "Stage 3",
                            stage == 4 ~ "Stage 4")) %>% 
-  ggplot(aes(x = as_factor(dose), y = n, fill=status)) +
+  ggplot(aes(x = as_factor(dose), 
+             y = n, 
+             fill = status)) +
   geom_col(position = "dodge") +
-  labs(x= "Dose", y="Total patients", color="Status") +
+  labs(x= "Dose", 
+       y="Total patients", 
+       color="Status") +
   ggtitle("Survival Per Treatment - Grouped By Cancer Stage") +
-  scale_fill_manual(values=c("#00AFBB", "#E7B800")) +
+  scale_fill_manual(values = c("#00AFBB", "#E7B800")) +
   theme_minimal() +
   facet_wrap(~stage)
 
 #Plot 5 - Reason of death per dose
 dataPlot5 <- data_clean_aug %>% 
-  filter(reasonDeath!="not dead" &
-           reasonDeath!="unknown cause") %>% 
-  group_by(reasonDeath,dose) %>% 
-  summarise(percentage=n()) %>% 
+  filter(reasonDeath != "not dead" &
+           reasonDeath != "unknown cause") %>% 
+  group_by(reasonDeath, dose) %>% 
+  summarise(percentage = n()) %>% 
   group_by(dose) %>% 
-  mutate(percentage=percentage/sum(percentage)*100)
+  mutate(percentage = percentage/sum(percentage)*100)
 
 #Prepare labels
 dose.labs <- c("Placebo", "Estrogen 0.2mg", "Estrogen 1mg", "Estrogen 5mg")
@@ -128,16 +160,17 @@ names(dose.labs) <- c("0", "0.2", "1", "5" )
 
 #Make the plot
 plot5 <-  dataPlot5 %>%
-  ggplot(aes(x="", y=percentage, fill=reasonDeath)) +
-  geom_bar(stat="identity", width=1, color="white" ) +
-  coord_polar("y", start=0) +
-  geom_text(size=2, 
+  ggplot(aes(x = "", y = percentage, fill = reasonDeath)) +
+  geom_bar(stat = "identity", width = 1, color = "white" ) +
+  coord_polar("y", start = 0) +
+  geom_text(size = 2, 
             aes(x = 1.65, label = str_c(round(percentage), "%")), 
             position = position_stack(vjust = 0.5)) +
-  theme_void()+ # remove background, grid, numeric labels
-  #scale_fill_brewer(palette = "Dark2") +
+  theme_void() + # remove background, grid, numeric labels
   labs(title = "Cause of death per treatment") +
-  facet_wrap(~ dose, nrow = 1, labeller = labeller(dose = dose.labs)) +
+  facet_wrap(~ dose, 
+             nrow = 1, 
+             labeller = labeller(dose = dose.labs)) +
   theme(plot.title = element_text(hjust = 0.5), 
         legend.position = "bottom", 
         legend.title = element_blank())
@@ -168,30 +201,30 @@ dataPlot6 <- full_join(dataPlot6, patients_per_dose, by = "dose")
 
 #Compute percentage
 dataPlot6 <- dataPlot6 %>% group_by(dose) %>% 
-  mutate(percentage=round((ppl_no/ppl_total)*100, 1))
+  mutate(percentage = round((ppl_no/ppl_total)*100, 1))
 
 #Make the plot
 plot6 <- dataPlot6 %>% 
-  ggplot(aes(x=ageGroup, y=percentage, fill = ageGroup)) +
-  geom_bar(stat="identity",position="dodge") +
+  ggplot(aes(x = ageGroup, y = percentage, fill = ageGroup)) +
+  geom_bar(stat = "identity",position = "dodge") +
   facet_wrap(~ dose, nrow = 1, 
              labeller = labeller(dose = dose.labs), 
              strip.position = "bottom") +
   theme(panel.background = element_blank(), 
         axis.line = element_line(colour = "black"),
-        axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_blank(),
         legend.position = "none",
         strip.placement = "outside",
         strip.background = element_blank(),
         plot.title = element_text(hjust = 0.5)) + 
   scale_y_continuous(expand = c(0,0), limit = c(0,59)) +
   scale_x_discrete(breaks = NULL) +
-  geom_text(aes(label=ageGroup, colour=ageGroup), 
-            vjust=0.5, 
-            hjust=0.5, 
-            size=2,
-            nudge_y= 1) +
+  geom_text(aes(label = ageGroup, colour=ageGroup), 
+            vjust = 0.5, 
+            hjust = 0.5, 
+            size = 2,
+            nudge_y = 1) +
   labs(title = "Survival rate based on treatment per age group", 
        y = "Percentage of alive patients")
 
@@ -201,13 +234,13 @@ dataPlot7 <- data_clean_aug %>%
   mutate(boneMetastase = case_when(boneMetastase == 0 ~ "No",
                                    boneMetastase == 1 ~"Yes")) %>% 
   count(stage,boneMetastase) %>% 
-  mutate(percentage = round(n*100/sum(n),1)) 
+  mutate(percentage = round(n*100/sum(n), 1)) 
 
 plot7 <- dataPlot7 %>%
-  ggplot(aes(x=stage,y=boneMetastase)) +
+  ggplot(aes(x = stage, y = boneMetastase)) +
   geom_tile(aes(fill = n), color = "white") +
-  geom_text (aes(label=n), vjust = -1) +
-  geom_text(aes(label = str_c(round(percentage,1),"%")), 
+  geom_text (aes(label = n), vjust = -1) +
+  geom_text(aes(label = str_c(round(percentage, 1), "%")), 
             vjust = 1) +
   scale_fill_gradient(low = "white", high = "#00AFBB") +
   theme_minimal() +
