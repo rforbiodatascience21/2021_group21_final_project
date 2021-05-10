a4997c96ba0e459ac3a58200a21560a3133d7887
gulsude <gulsudedemircan@gmail.com>
2021-05-10 13:30:55 +0200

    organize file according to the tidyverse style.

--- R/06_analysis_iii.R
+++ R/06_analysis_iii.R
@@ -14,19 +14,21 @@ data_kmeans <- read_tsv(file = "data/03_data_clean_num.tsv.gz")
 
 ## kmean Clustering -------------------------------------------------------
 #prepare a table withput y (status alive/death)
-data_kmeans_noLabel <- data_kmeans %>% select(-status)
+data_kmeans_noLabel <- data_kmeans %>% 
+  select(-status)
 
 #retire dose, diastolic and sistolic, acid Phosphatase since 
 #they were found to not have goow correlation to status
 dropCol <- c("diastolicBP","dose","systolicBP","acidPhosphatase")
 
-data_kmeans_noLabel <- data_kmeans_noLabel %>% select(-one_of(dropCol))
+data_kmeans_noLabel <- data_kmeans_noLabel %>% 
+  select(-one_of(dropCol))
 
 #apply kmeans model with k=2 (status alive/death)
 kClust <- kmeans(data_kmeans_noLabel, 
-                 centers=2)
-kClust
-summary(kClust)
+                 centers = 2)
+
+#summary(kClust)
 
 #compare with original status to check performance
 #create a "confussion" matrix
@@ -39,7 +41,7 @@ kmean_aug <- augment(kClust, data_kmeans) %>%
   mutate(percentage = n*100/sum(n))
 
 #plotting as a confussion matrix
-plot1 <- ggplot(kmean_aug, aes(x=status, y=cluster)) +
+plot1 <- ggplot(kmean_aug, aes(x = status, y = cluster)) +
   geom_tile(aes(fill = n), color = "white") +
   geom_text(aes(label = n), vjust = -1) +
   geom_text(aes(label = str_c(round(percentage, 1), "%")), vjust = 1) +
